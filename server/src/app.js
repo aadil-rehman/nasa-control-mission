e7f940bfeb026d52ef5da4f6b9dc433ff39e50d3
Moh Aadil <aadilrehman.development@gmail.com>
2024-09-05 01:15:03 +0530

    add: launches API - controller, router and launches model

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const planetRouter = require("./routes/planets.router");
+const planetRouter = require("./routes/planets/planets.router");
+const path = require("path");
+const morgan = require("morgan");
+const { launchesRouter } = require("./routes/launches/launches.router");
 
 const app = express();
 
@@ -9,7 +12,16 @@ app.use(
 		origin: "http://localhost:3000",
 	})
 );
-app.use(express.json());
-app.use(planetRouter);
 
+app.use(morgan("combined"));
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.use(planetRouter);
+app.use(launchesRouter);
+
+app.get("/*", (req, res) => {
+	res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 module.exports = app;
